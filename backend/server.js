65b758ddc956a4826cdfa1fed152e04b8752008e
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-07-06 23:50:02 -0600

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const morgan = require('morgan')
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
 
-const recipeRoutes = require('./routes/recipes');
+const recipeRoutes = require("./routes/recipes");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("connected to db");
+  app.listen(process.env.PORT, () => {
+    console.log("app is listening at port 4000...");
+  });
+});
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    return res.json({hi : "Hello World"})
-})
+app.get("/", (req, res) => {
+  return res.json({ hi: "Hello World" });
+});
 
 app.use("/api/recipes", recipeRoutes);
-
-
-app.listen(process.env.PORT, () => {
-    console.log('app is listening at port 4000...');
-})
