f3f9ff0a5b2f88ad2bc9b81a99deeb3a1b3c484f
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-07-11 14:21:52 -0600

    recipe send to server with axios

--- frontend/src/pages/RecipeForm.tsx
+++ frontend/src/pages/RecipeForm.tsx
@@ -1,44 +1,91 @@
 import { useState } from "react";
 import plus from "../assets/plus.svg";
 import Ingredients from "../components/Ingredients";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 
 export default function RecipeForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [ingredients, setIngredients] = useState<string[]>([]);
   const [newIngredient, setNewIngredient] = useState("");
+  const [error, setError] = useState<string[]>([]);
+  const navigate = useNavigate();
 
   const addIngredient = () => {
     setIngredients((prevState) => [newIngredient, ...prevState]);
     setNewIngredient("");
   };
 
-  const createRecipe = (e : React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const recipe = {
-      title,
-      description,
-      ingredients
+  const createRecipe = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const recipe = {
+        title,
+        description,
+        ingredients,
+      };
+
+      //server request
+      const res = await axios.post("http://localhost:4000/api/recipes", recipe);
+      if (res.status === 200) {
+        toast.success("Recipe created successfully!");
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      }
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        setError(Object.keys(e.response?.data.errors));
+      } else {
+        console.error(e);
+      }
     }
-    console.log(recipe);
-  }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-50 to-pink-100 py-10 px-2">
-      <form className="bg-white rounded-2xl shadow-xl p-8 w-full max-w-lg border border-pink-100" onSubmit={createRecipe}>
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
+      ;
+      <form
+        className="bg-white rounded-2xl shadow-xl p-8 w-full max-w-lg border border-pink-100"
+        onSubmit={createRecipe}
+      >
         <h2 className="text-2xl font-bold text-pink-600 mb-6 text-center flex items-center gap-2 justify-center">
           <span role="img" aria-label="cake">
             🍰
           </span>{" "}
           Add a New Recipe
         </h2>
+        {!!error.length && (
+          <div className="mb-6 bg-pink-100 border border-pink-300 text-pink-700 rounded-lg px-4 py-3">
+            <ul className="list-disc list-inside">
+              {error.map((err, i) => (
+                <li key={i}>{err} is invalid.</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <div className="mb-4">
           <label className="block text-sm font-semibold mb-1 text-gray-700">
             Title
           </label>
           <input
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             className="w-full px-4 py-2 border border-pink-200 rounded focus:outline-none focus:ring-2 focus:ring-pink-300"
             placeholder="e.g. Strawberry Cake"
@@ -50,8 +97,8 @@ export default function RecipeForm() {
             Description
           </label>
           <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             className="w-full px-4 py-2 border border-pink-200 rounded focus:outline-none focus:ring-2 focus:ring-pink-300 resize-none min-h-[80px]"
             placeholder="How to make this recipe..."
             // required
@@ -98,7 +145,7 @@ export default function RecipeForm() {
           />
         </div>
         <div className="flex gap-5 items-center">
-        <button
+          <button
             type="submit"
             className="w-full bg-white border border-pink-300 text-pink-500 hover:bg-pink-50 font-bold py-2 rounded-lg shadow transition-colors duration-200 text-lg mt-2"
           >
@@ -110,7 +157,6 @@ export default function RecipeForm() {
           >
             Submit Recipe
           </button>
-          
         </div>
       </form>
     </div>
