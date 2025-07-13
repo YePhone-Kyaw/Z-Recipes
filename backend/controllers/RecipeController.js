6bdf0dd232510f696a16beff05a557853551e90e
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-07-12 22:19:25 -0600

    pagination implementation

--- backend/controllers/RecipeController.js
+++ backend/controllers/RecipeController.js
@@ -1,10 +1,16 @@
 const Recipe = require("../models/Recipe");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const RecipeController = {
   index: async (req, res) => {
+    const limit = 6;
+    const page = req.query.page || 1;
     try {
-      const recipes = await Recipe.find().sort({ createdAt: -1 });
+      const recipes = await Recipe.find()
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort({ createdAt: -1 });
+
       return res.json(recipes);
     } catch (e) {
       return res.status(400).json({ message: "Error at retrieving data" });
@@ -25,12 +31,12 @@ const RecipeController = {
       const id = req.params.id;
       // validate the valid id or not
       if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message : "Not a valid id" });
+        return res.status(400).json({ message: "Not a valid id" });
       }
       const showRecipe = await Recipe.findById(id);
       // check the recipe exists in db or not even though the id might be valid format
       if (!showRecipe) {
-        return res.status(404).json({ message : "Recipe not found" });
+        return res.status(404).json({ message: "Recipe not found" });
       }
       return res.json(showRecipe);
     } catch (e) {
@@ -39,34 +45,34 @@ const RecipeController = {
   },
   destroy: async (req, res) => {
     try {
-        const id = req.params.id;
-        // validate the valid id or not
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          return res.status(400).json({ message : "Not a valid id" });
-        }
-        const showRecipe = await Recipe.findByIdAndDelete(id);
-        // check the recipe exists in db or not even though the id might be valid format
-        if (!showRecipe) {
-          return res.status(404).json({ message : "Recipe not found" });
-        }
-        return res.json(showRecipe);
-      } catch (e) {
-        return res.status(500).json({ message: "Internal server error" });
+      const id = req.params.id;
+      // validate the valid id or not
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Not a valid id" });
       }
+      const showRecipe = await Recipe.findByIdAndDelete(id);
+      // check the recipe exists in db or not even though the id might be valid format
+      if (!showRecipe) {
+        return res.status(404).json({ message: "Recipe not found" });
+      }
+      return res.json(showRecipe);
+    } catch (e) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
   },
   update: async (req, res) => {
     try {
       const id = req.params.id;
       // validate the valid id or not
       if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message : "Not a valid id" });
+        return res.status(400).json({ message: "Not a valid id" });
       }
       const showRecipe = await Recipe.findByIdAndUpdate(id, {
-        ...req.body
+        ...req.body,
       });
       // check the recipe exists in db or not even though the id might be valid format
       if (!showRecipe) {
-        return res.status(404).json({ message : "Recipe not found" });
+        return res.status(404).json({ message: "Recipe not found" });
       }
       return res.json(showRecipe);
     } catch (e) {
