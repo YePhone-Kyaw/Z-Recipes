84593dc522b21e553dd6b867a726342381f8641f
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-07-21 01:09:08 -0600

    user register api implementation

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,9 +1,29 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 const UserController = {
   login: (req, res) => {
     return res.json({ message: "register api hit" });
   },
-  register: (req, res) => {
-    return res.json({ message: "register api hit" });
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        throw new Error("User already exists");
+      }
+      const salt = await bcrypt.genSalt();
+      const hashValue = await bcrypt.hash(password, salt);
+
+      const user = await User.create({
+        name,
+        email,
+        password: hashValue,
+      });
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
   },
 };
 
