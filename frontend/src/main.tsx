1929f533fa5bd614bb5ffd41ec42e4ff13155832
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-08-05 22:32:00 -0600

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
@@ -52,7 +51,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
-      {/* <App /> */}
       <RouterProvider router={router} />
     </AuthContextProvider>
   </StrictMode>
