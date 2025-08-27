bfb12c869223a739f79a439aa4e21f70aa2b9a94
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-08-26 23:59:47 -0600

    Reusable send email helper

---
+++ backend/helpers/sendEmail.js
@@ -0,0 +1,26 @@
+const sendEmail = ({ fileName, data, from, to, subject }) => {
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "5779666eb87085",
+          pass: "ab6ee6a39a7549",
+        },
+      });
+    
+      ejs.renderFile(
+        "./views/"+fileName+".ejs",
+        data,
+        async (error, dataString) => {
+          const info = await transport.sendMail({
+            from,
+            to,
+            subject,
+            html: dataString,
+          });
+    
+          console.log("Message sent:", info.messageId);
+          return res.send("Email already send");
+        }
+      );
+}
